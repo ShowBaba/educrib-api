ab2b2e4a7f822c913400247c9578befd8307ed0e
ShowBaba <samwise858@gmail.com>
2020-10-05 05:07:07 +0100

    update view

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 // TODO: Serve a documentation page
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Edu Crib' });
+  res.render('index', { title: 'Edu Crib'});
 });
 
 module.exports = router;
